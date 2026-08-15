f464cb32be2d889d1eba4921e91a89284ce5072c
theoscott1002 <ariyotheophilus@gmail.com>
2026-08-15 12:48:25 +0100

    Created tfstate s3 bucket

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,7 @@ output "cloudfront_url" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.frontend.bucket
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.frontend.id
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  
+  backend "s3" {
+  bucket       = "student-management-tfstate-716482800050"
+  key          = "student-management/terraform.tfstate"
+  region       = "us-east-1"
+  use_lockfile = true
+}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
